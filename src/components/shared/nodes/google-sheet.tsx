e47e99d35b73e62c79bfa8d870db071be7ddba4d
sanjeeb <code.sanjeeb@gmail.com>
2024-08-24 18:37:13 +0530

    #Google sheet

--- src/components/shared/nodes/google-sheet.tsx
+++ src/components/shared/nodes/google-sheet.tsx
@@ -3,20 +3,52 @@ import { memo, useState } from "react";
 import CustomNode from "../custom-node";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import Papa from 'papaparse';
+import { useReactFlow } from "@xyflow/react";
+import toast from "react-hot-toast";
 
-const GoogleSheet = (props: any) => {
-    const [input, setInput] = useState("");
+const GoogleSheet = ({id, data, ...props}: any) => {
+    const [input, setInput] = useState(data?.url || "");
+    const [parsing , setParsing] = useState(false);
+    const { updateNodeData } = useReactFlow();
+
+    const handleLoad = () => {
+        try {
+            const sheet_id = input;
+            if (sheet_id) {
+                setParsing(true)
+                Papa.parse(`https://docs.google.com/spreadsheets/d/e/${sheet_id}/pub?output=csv`, {
+                    worker:true,
+                    download:true,
+                    complete: (result) => {
+                        setParsing(false)
+                        console.log(result)
+                        updateNodeData(id, { dataset: result?.data || [], url: sheet_id })
+                    },
+                    error:(err)=>{
+                        toast.error(err?.message || "Error while parsing.");
+                    },
+                    header: true,
+                });
+            }else{
+                toast.error("Please add URL.")
+            }
+        } catch (err) {
+            console.error(err)
+        }
+    };
+
     return (
-        <CustomNode title="Google sheets" enableTarget={false} {...props}>
+        <CustomNode title="Google sheets" enableTarget={false} input={parsing ? "Parsing...":"Dataset : " + data?.dataset.length || 0} id={id} {...props}>
             <div className='grid place-content-center '>
-                <p>Google sheet id:</p>
+                <p>Google sheet downloadable ID:</p>
                 <Input
                     type='text'
                     value={input}
                     onChange={e => { e.preventDefault(); setInput(e.target.value) }}
                     className='my-2 min-w-64 border-2 border-primary'
                 />
-                <Button className='w-fit'>Load</Button>
+                <Button className='w-fit' onClick={handleLoad} disabled={parsing}>{parsing ? "Loading...":"Load"}</Button>
             </div>
         </CustomNode >
     );
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import AppRoutes from './routes.tsx'
 import './index.css'
 import { AuthProvider } from './lib/auth-provider.tsx'
+import  { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
       <AppRoutes />
+      <Toaster />
     </AuthProvider >
   </React.StrictMode>,
 )
