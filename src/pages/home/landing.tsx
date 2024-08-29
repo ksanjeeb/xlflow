d4112464ebf992b0f59ca324df5f91a70ee1d241
sanjeeb <code.sanjeeb@gmail.com>
2024-08-30 00:21:48 +0530

    Update landing.tsx

--- src/pages/home/landing.tsx
+++ src/pages/home/landing.tsx
@@ -66,7 +66,7 @@ function HomePage() {
 
     return (
         <>
-            <section className="w-full py-12 md:py-24 lg:py-28 xl:py-40">
+            <section className="w-full py-12 md:py-24 lg:py-28 xl:pb-40">
                 <div className="container px-4 md:px-6">
                     <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
                         <div className="flex flex-col justify-center space-y-4">
